60cdc3072b93faccb8a2ac9acc05541014acb05b
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-26 19:00:59 -0300

    consuming the messages with product event consumer

---
+++ src/main/java/com/github/pedroluiznogueira/microservicesconsumer/service/ProductEventConsumer.java
@@ -0,0 +1,47 @@
+package com.github.pedroluiznogueira.microservicesconsumer.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.pedroluiznogueira.microservicesconsumer.model.Envelope;
+import com.github.pedroluiznogueira.microservicesconsumer.model.ProductEvent;
+import com.github.pedroluiznogueira.microservicesconsumer.model.SnsMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+
+@Service
+public class ProductEventConsumer {
+    private static final Logger log = LoggerFactory.getLogger(
+            ProductEventConsumer.class);
+
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public ProductEventConsumer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @JmsListener(destination = "${aws.sqs.queue.product.events.name}")
+    public void receiveProductEvent(TextMessage textMessage) throws JMSException, IOException {
+
+        SnsMessage snsMessage = objectMapper.readValue(textMessage.getText(),
+                SnsMessage.class);
+
+        Envelope envelope = objectMapper.readValue(snsMessage.getMessage(),
+                Envelope.class);
+
+        ProductEvent productEvent = objectMapper.readValue(
+                envelope.getData(), ProductEvent.class);
+
+        log.info("Product event received - Event: {} - ProductId: {} - " +
+                        "MessageId: {}", envelope.getEventType(),
+                productEvent.getProductId(), snsMessage.getMessageId());
+    }
+}
