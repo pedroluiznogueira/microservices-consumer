f14aa8dabdeb3390fc57433123abaac98a3a64dd
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-27 20:48:27 -0300

    product event log entity and controller

---
+++ src/main/java/com/github/pedroluiznogueira/microservicesconsumer/controller/ProductEventLogController.java
@@ -0,0 +1,50 @@
+package com.github.pedroluiznogueira.microservicesconsumer.controller;
+
+import com.github.pedroluiznogueira.microservicesconsumer.model.ProductEventLogDto;
+import com.github.pedroluiznogueira.microservicesconsumer.repository.ProductEventLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@RestController
+@RequestMapping("/api")
+public class ProductEventLogController {
+
+    private ProductEventLogRepository productEventLogRepository;
+
+    @Autowired
+    public ProductEventLogController(ProductEventLogRepository productEventLogRepository) {
+        this.productEventLogRepository = productEventLogRepository;
+    }
+
+    @GetMapping("/events")
+    public List<ProductEventLogDto> getAllEvents() {
+        return StreamSupport
+                .stream(productEventLogRepository.findAll().spliterator(), false)
+                .map(ProductEventLogDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/events/{code}")
+    public List<ProductEventLogDto> findByCode(@PathVariable String code) {
+        return productEventLogRepository.findAllByPk(code)
+                .stream()
+                .map(ProductEventLogDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/events/{code}/{event}")
+    public List<ProductEventLogDto> findByCodeAndEventType(@PathVariable String code,
+                                                           @PathVariable String event) {
+        return productEventLogRepository.findAllByPkAndSkStartsWith(code, event)
+                .stream()
+                .map(ProductEventLogDto::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/github/pedroluiznogueira/microservicesconsumer/model/ProductEventLogDto.java
@@ -0,0 +1,40 @@
+package com.github.pedroluiznogueira.microservicesconsumer.model;
+
+import com.github.pedroluiznogueira.microservicesconsumer.enums.EventType;
+
+public class ProductEventLogDto {
+
+    private final String code;
+    private final EventType eventType;
+    private final long productId;
+    private final String username;
+    private final long timestamp;
+
+    public ProductEventLogDto(ProductEventLog productEventLog) {
+        this.code = productEventLog.getPk();
+        this.eventType = productEventLog.getEventType();
+        this.productId = productEventLog.getProductId();
+        this.username = productEventLog.getUsername();
+        this.timestamp = productEventLog.getTimestamp();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
